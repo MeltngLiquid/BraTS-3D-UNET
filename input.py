185f122e440c736e258ab5ee3cfb567106e59f43
MeltngLiquid <bm23btech11006@iith.ac.in>
2025-08-03 10:45:46 +0530

    input.py

--- input.py
+++ input.py
@@ -1,24 +1,4 @@
-# https://youtu.be/oB35sV1npVI
-"""
-Use this code to get your BRATS 2020 dataset ready for semantic segmentation. 
-Code can be divided into a few parts....
 
-#Combine 
-#Changing mask pixel values (labels) from 4 to 3 (as the original labels are 0, 1, 2, 4)
-#Visualize
-
-
-https://pypi.org/project/nibabel/
-
-All BraTS multimodal scans are available as NIfTI files (.nii.gz) -> commonly used medical imaging format to store brain imagin data obtained using MRI and describe different MRI settings
-
-T1: T1-weighted, native image, sagittal or axial 2D acquisitions, with 1–6 mm slice thickness.
-T1c: T1-weighted, contrast-enhanced (Gadolinium) image, with 3D acquisition and 1 mm isotropic voxel size for most patients.
-T2: T2-weighted image, axial 2D acquisition, with 2–6 mm slice thickness.
-FLAIR: T2-weighted FLAIR image, axial, coronal, or sagittal 2D acquisitions, 2–6 mm slice thickness.
-
-#Note: Segmented file name in Folder 355 has a weird name. Rename it to match others.
-"""
 
 
 import numpy as np
@@ -30,25 +10,11 @@ from tifffile import imsave
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
-##########################
-#This part of the code to get an initial understanding of the dataset.
-#################################
-#PART 1: Load sample images and visualize
-#Includes, dividing each image by its max to scale them to [0,1]
-#Converting mask from float to uint8
-#Changing mask pixel values (labels) from 4 to 3 (as the original labels are 0, 1, 2, 4)
-#Visualize
-###########################################
-#View a few images
-
-#Note: Segmented file name in Folder 355 has a weird name. Rename it to match others.
 
 TRAIN_DATASET_PATH = 'BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/'
-#VALIDATION_DATASET_PATH = 'BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData'
 
 test_image_flair=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_355/BraTS20_Training_355_flair.nii').get_fdata()
 print(test_image_flair.max())
-#Scalers are applied to 1D so let us reshape and then reshape back to original shape. 
 test_image_flair=scaler.fit_transform(test_image_flair.reshape(-1, test_image_flair.shape[-1])).reshape(test_image_flair.shape)
 
 
@@ -92,24 +58,12 @@ plt.show()
 
 
 
-##################################################
-#PART 2: Explore the process of combining images to channels and divide them to patches
-#Includes...
-#Combining all 4 images to 4 channels of a numpy array.
-#
-################################################
-#Flair, T1CE, annd T2 have the most information
-#Combine t1ce, t2, and flair into single multichannel image
 
 combined_x = np.stack([test_image_flair, test_image_t1ce, test_image_t2], axis=3)
 
-#Crop to a size to be divisible by 64 so we can later extract 64x64x64 patches. 
-#cropping x, y, and z
-#combined_x=combined_x[24:216, 24:216, 13:141]
 
-combined_x=combined_x[56:184, 56:184, 13:141] #Crop to 128x128x128x4
+combined_x=combined_x[56:184, 56:184, 13:141] 
 
-#Do the same for mask
 test_mask = test_mask[56:184, 56:184, 13:141]
 
 n_slice=random.randint(0, test_mask.shape[2])
@@ -132,34 +86,19 @@ plt.show()
 
 imsave('BraTS2020_TrainingData/combined255.tif', combined_x)
 np.save('BraTS2020_TrainingData/combined255.npy', combined_x)
-#Verify image is being read properly
-#my_img=imread('BraTS2020_TrainingData/combined255.tif')
 
 my_img=np.load('BraTS2020_TrainingData/combined255.npy')
 
 test_mask = to_categorical(test_mask, num_classes=4)
-####################################################################
-#####################################
-#End of understanding the dataset. Now get it organized.
-#####################################
 
-#Now let us apply the same as above to all the images...
-#Merge channels, crop, patchify, save
-#GET DATA READY =  GENERATORS OR OTHERWISE
-
-#Keras datagenerator does ntot support 3d
-
-# # # images lists harley
-#t1_list = sorted(glob.glob('BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/*/*t1.nii'))
 t2_list = sorted(glob.glob('BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/*/*t2.nii'))
 t1ce_list = sorted(glob.glob('BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/*/*t1ce.nii'))
 flair_list = sorted(glob.glob('BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/*/*flair.nii'))
 mask_list = sorted(glob.glob('BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/*/*seg.nii'))
 
-#Each volume generates 18 64x64x64x4 sub-volumes. 
-#Total 369 volumes = 6642 sub volumes
 
-for img in range(len(t2_list)):   #Using t1_list as all lists are of same size
+
+for img in range(len(t2_list)): 
     print("Now preparing image and masks number: ", img)
       
     temp_image_t2=nib.load(t2_list[img]).get_fdata()
@@ -193,13 +132,8 @@ for img in range(len(t2_list)):   #Using t1_list as all lists are of same size
         np.save('BraTS2020_TrainingData/input_data_3channels/masks/mask_'+str(img)+'.npy', temp_mask)
         
     else:
-        print("I am useless")   
+        print("not usefull")   
    
-     
-################################################################
-#Repeat the same from above for validation data folder OR
-#Split training data into train and validation
-
 """
 Code for splitting folder into train, test, and val.
 Once the new folders are created rename them and arrange in the format below to be used
@@ -211,7 +145,6 @@ import splitfolders  # or import split_folders
 
 input_folder = 'BraTS2020_TrainingData/input_data_3channels/'
 output_folder = 'BraTS2020_TrainingData/input_data_128/'
-# Split with a ratio.
-# To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
 splitfolders.ratio(input_folder, output=output_folder, seed=42, ratio=(.75, .25), group_prefix=None) # default values
 ########################################
+
